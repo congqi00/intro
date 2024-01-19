e58d2b715942ad5e6d108b48525f5581882a0f09
congqi00 <157077124+congqi00@users.noreply.github.com>
2024-01-19 00:55:38 +0000

    try parallax effect demo

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var scene=document.getElementById("scene");
+var parallax=new Parallax(scene);
+var modal=document.getElementsByClassName("modal")[0];
+var info=document.getElementsByClassName("about")[0];
+info.addEventListener("click",function(){modal.classList.add("active")});
+var close=document.getElementsByClassName("modal__card__close")[0];
+close.addEventListener("click",function(){modal.classList.remove("active")});
